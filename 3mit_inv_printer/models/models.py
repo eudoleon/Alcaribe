9042daa7e9452328d2ed5642df39958c623d80c5
Eudo Leon <78383346+eudoleon@users.noreply.github.com>
2024-11-15 12:24:32 -0400

    Update models.py

--- 3mit_inv_printer/models/models.py
+++ 3mit_inv_printer/models/models.py
@@ -80,7 +80,7 @@ class AccountMove(models.Model):
         ticket['items'] = items
 
         # Verificar si existen pagos asociados a la factura
-        ticket['pagos'] = [{'codigo': '20' if self.es_pago_en_divisa else '01', 'nombre': 'EFECTIVO', 'monto': self.amount_total}]
+        ticket['pagos'] = [{'codigo': '20' if self.es_pago_en_divisa else '01', 'nombre': 'EFECTIVO', 'monto': self.amount_residual_signed}]
 
         return {
             'res_model': 'account.move',
