5fc1271bad58b90b14a412190be99ce84f1f95cf
andresecas15 <68520116+leojsm26@users.noreply.github.com>
2024-11-29 12:21:28 -0400

    Update __manifest__.py

--- 3mit_inv_printer/__manifest__.py
+++ 3mit_inv_printer/__manifest__.py
@@ -6,7 +6,7 @@
         Emite factura a rest-api de impresora fiscal""",
 
     'description': """
-        V.1.0.0 - Impresora fiscal sin localizacion:
+        V.1.0.0.1 - Impresora fiscal sin localizacion:
                     - Facturas de ventas.
         
     """,
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['account','10n_ve_dual_currency_bs'],
