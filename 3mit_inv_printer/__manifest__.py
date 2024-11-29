25b9de6108a72bcfa60295917133419500c60773
andresecas15 <68520116+leojsm26@users.noreply.github.com>
2024-11-29 10:41:07 -0400

    Update __manifest__.py

--- 3mit_inv_printer/__manifest__.py
+++ 3mit_inv_printer/__manifest__.py
@@ -6,7 +6,7 @@
         Emite factura a rest-api de impresora fiscal""",
 
     'description': """
-        V.1.0.0.1 - Impresora fiscal sin localizacion:
+        V.1.0.0 - Impresora fiscal sin localizacion:
                     - Facturas de ventas.
         
     """,
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1.1',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['account','10n_ve_dual_currency_bs'],
@@ -28,14 +28,13 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/nota_credito.xml',
-        'views/options.xml'
+        # 'views/options.xml'
     ],
     'assets': {
         'web.assets_backend': [
-            #"3mit_print_server/static/src/js/dialog.js",
-            "3mit_print_server/static/src/js/print_nc.js",
-            "3mit_print_server/static/src/js/print_factura.js",
-            "3mit_print_server/static/src/js/print_options.js",
+            "3mit_inv_printer/static/src/js/print_nc.js",
+            # "3mit_inv_printer/static/src/js/print_options.js",
+            "3mit_inv_printer/static/src/js/printer.js",
         ],
     },
     # only loaded in demonstration mode
