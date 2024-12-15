5069ffc46ac7a3f6c8bb86c92a9daa4a61be3602
andresecas15 <68520116+leojsm26@users.noreply.github.com>
2024-12-15 17:05:54 -0400

    Update __manifest__.py

--- 3mit_inv_printer/__manifest__.py
+++ 3mit_inv_printer/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '16.0',
 
     # any module necessary for this one to work correctly
     'depends': ['account'],
