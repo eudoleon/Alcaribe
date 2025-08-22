bd6ccf25e964cc42a462823ab4ac00342c4520c7
andresecas15 <leonardojsemprunm@gmail.com>
2025-08-22 14:36:28 -0400

    delete

--- tax_withholdings/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import account_move
-from . import account_move_line
-from . import account_tax
-from . import account_partial_reconcile
-from . import account_payment
